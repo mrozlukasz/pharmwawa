d2221c22996ed3247b1eadb0c322abddcad8be99
Łukasz Mróz <l.mroz88@gmail.com>
2016-10-23 13:09:52 +0200

    Bot improvements

--- app/bots.js
+++ app/bots.js
@@ -24,6 +24,55 @@ module.exports = function(router, TOKEN) {
         });
     }
 
+    function sendGenericMessage(sender) {
+        var messageData = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "First card",
+                        "subtitle": "Element #1 of an hscroll",
+                        "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                        "buttons": [{
+                            "type": "web_url",
+                            "url": "https://www.messenger.com",
+                            "title": "web url"
+                        }, {
+                            "type": "postback",
+                            "title": "Postback",
+                            "payload": "Payload for first element in a generic bubble",
+                        }]
+                    }, {
+                        "title": "Second card",
+                        "subtitle": "Element #2 of an hscroll",
+                        "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Postback",
+                            "payload": "Payload for second element in a generic bubble",
+                        }]
+                    }]
+                }
+            }
+        };
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {access_token:token},
+            method: 'POST',
+            json: {
+                recipient: {id:sender},
+                message: messageData
+            }
+        }, function(error, response, body) {
+            if (error) {
+                console.log('Error sending messages: ', error)
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error)
+            }
+        })
+    }
+
 // for Facebook verification
     router
         .get('/', function (req, res) {
@@ -35,12 +84,17 @@ module.exports = function(router, TOKEN) {
         })
 // for sending messages
         .post('/', function (req, res) {
-            var messaging_events = req.body.entry[0].messaging;
-            messaging_events.forEach(function (event) {
+            req.body.entry[0].messaging.forEach(function (event) {
                 var sender = event.sender.id;
                 if (event.message && event.message.text) {
                     var text = event.message.text;
-                    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+                    console.log(event.message.text);
+                    if (text === 'Generic') {
+                        sendGenericMessage(sender);
+                    } else {
+                        sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+                    }
+
                 }
             });
             res.sendStatus(200)
